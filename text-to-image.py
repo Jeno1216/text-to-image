6e1e7882942f606fcbad1bf8df8cd062ea98a95f
Jeno1216 <80746647+Jeno1216@users.noreply.github.com>
2023-04-08 08:01:29 +0800

    Update text-to-image.py

--- text-to-image.py
+++ text-to-image.py
@@ -20,7 +20,12 @@ def generate_image(prompt):
     st.image(image_url, use_column_width=True)
 
 # Set up Streamlit app
-st.title("Text to Image")
+st.title("Welcome to Imagination Visualizer.")
+
+" Unleash your imagination and bring it to life with our cutting-edge image generation application!"
+"Simply enter your thoughts into the text box, and let our software create stunning visualizations that bring your ideas to life. "
+"From dreamy landscapes to fantastical creatures, the possibilities are endless. So why wait? Start generating your own unique and captivating images today!."
+
 
 # Get user input
 text_input = st.text_input("Enter text:")
